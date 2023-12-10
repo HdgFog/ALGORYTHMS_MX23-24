d8e18f4e3e15b6d29e05ab1a6b7df1f9b6281baf
EgorLubnin <143064754+EgorLubnin@users.noreply.github.com>
2023-12-10 16:39:00 +0500

    Переписал функцию определения типа шарнира на c++

---
+++ Листинги 7.19-7.21/Листинг 7.21/Листинг 7.21/process_vertex_late.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+void process_vertex_late(int v) {
+    bool root;
+    int time_v;
+    int time_parent;
+
+    if (parent[v] == -1) {
+        if (tree_out_degree[v] > 1) {
+            std::cout << "root articulation vertex: " << v << std::endl;
+        }
+        return;
+    }
+
+    root = (parent[parent[v]] == -1);
+
+    if (!root) {
+        if (reacheble_ancestor[v] == parent[v]) {
+            std::cout << "parent articulation vertex: " << parent[v] << std::endl;
+        }
+
+        if (reachabe_ancestor[v] == v) {
+            std::cout << "bridge articulation vertex: " << parent[v] << std::endl;
+
+            if (tree_out_degree[v] > 0) {
+                std::cout << "bridge articulation vertex: " << v << std::endl;
+            }
+        }
+    }
+
+    time_v = entry_time[rechable_ancestor[v]];
+    time_parent = entry_time[rechable_ancestor[parent[v]]];
+    if (time_v < time_parent) {
+        rechable_ancestor[parent[v]] = rechable_ancestor[v];
+    }
+}
+
